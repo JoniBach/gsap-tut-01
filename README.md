9372c31e21b61d9c462366d6ee4a26991a5c1cd1
student <student@James-MacBook-Pro.local>
2020-11-14 21:31:16 +0000

    Committing completed tutorial

--- README.md
+++ README.md
@@ -1,70 +1,24 @@
-# Getting Started with Create React App
+# Learning SCSS and Green Sock
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project was built following a tutorial...
+[How To Code Animations in React.js with Greensock (GSAP)](https://youtu.be/H_VnkjiodO4).
 
-## Available Scripts
+# What I learned
 
-In the project directory, you can run:
+This tutorial was my first attempt at getting to know greensock as well as being my first propper go at implimenting scss. 
+-
+I learned how to make an animated hero (A large image presented at the header of the file) as well as how to incorperate GSAP into a react application. 
 
-### `npm start`
+# What I liked
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+I thought the tutorial was well delivered and highlighted some basics required to get me started with gsap. 
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+While the tutorial assumed a pretty standard level of knowledge with all the basic tools (i.e. JS, React, HTML/JSX, CSS) it gave me a good base line to start working with GSAP and assumed very little on that front
 
-### `npm test`
+# What I dissliked
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+Personally, I think the tutorial was a little to quick to follow at some stages and feel that the a few words here or there got lost to fumbled diction. Otherwise i enjoyed it over all.
 
-### `npm run build`
+# What I took away
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Making a Hero was a great way to try out a new animation lib as it is a chance to get pretty creative. I think i might try making some more Heros in the future.
--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+// our hooks
+import React, { useEffect, useRef, useFetch } from "react";
+import "./App.scss";
+
+// for easing
+import { TweenMax, TimelineLite, Power3 } from 'gsap'
+
+// asset
+import arrow from './images/arrow-right.svg'
 
 function App() {
+  // giving our variables access to the dom
+  let app = useRef(null);
+  let images = useRef(null)
+  let content = useRef(null)
+  let tl = new TimelineLite({ delay: .8});
+
+  // running our hero start up sequence when the app loads
+  useEffect(() => {
+    // Images
+    const firstCat = images.firstElementChild; // or children[0]
+    const secondCat = images.lastElementChild;
+    
+    //content
+    const headlineFirst = content.children[0].children[0];
+    const headlineSecond = headlineFirst.nextSibling;
+    const headlineThird = headlineSecond.nextSibling;
+    const contentP = content.children[1];
+    const contentButton = content.children[2];
+
+    // remove initial flash
+    TweenMax.to(app, 0, { css: { visibility: 'visible' } })
+
+    //Images Animation
+    tl.from(firstCat, 1.2, { y: 1280, ease: Power3.easeOut }, 'Start')
+      .from(firstCat.firstElementChild, 2, { scale: 1.6, ease: Power3.easeOut }, .2)
+      .from(secondCat, 1.4, { y: 1280, ease: Power3.easeOut }, .2)
+      .from(secondCat.firstElementChild, 2, { scale: 1.6, ease: Power3.easeOut }, .2)
+
+     //Content Animation
+     tl.staggerFrom([headlineFirst.children, headlineSecond.children, headlineThird.children ], 1, {
+      y: 44,
+      ease:Power3.easeOut,
+      delay: .8
+    }, .15, 'Start')
+    .from(contentP, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 1.4)
+    .from(contentButton, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 1.6)
+
+  
+    }, [tl])
+    // facts from https://iheartcats.com/10-things-you-didnt-know-about-kittens/
+  const kittenFacts = [
+    "Kittens eyes open at around 1 week of age, but they can’t see or hear clearly until they are 2-3 weeks old. This makes them completely dependent on mom for their tiny little lives.",
+    "Kittens conserve their energy by sleeping an average of 18 hours a day! That gives them plenty of fuel for growing strong and quickly into the adult cats we know so well. Even more interesting is the fact that a certain growth hormone is only released during sleep, which is why they must rest to grow.",
+    "Even newborn kittens can purr and can often be heard doing so while they’re nursing or their mothers are cleaning them. While their squeaks and squeals are much more obvious, if you listen closely you’ll hear their content little motors running.",
+    "Kittens can see color once they get their full vision, although they won’t see them as well as humans do. Because of this, they rely very much on a strong sense of smell that develops when their eyesight and hearing does.",
+    "Kittens can be right- or left-pawed, the same way people are right- or left-handed. If you watch closely, you’ll be able to determine whether your kitten uses his right or left paw when checking out new things.",
+    "Cats and kittens can fit their bodies almost anywhere they can squeeze their heads into, so be sure to exercise the utmost care and caution when kitten-proofing your house. Their adorable little heads can fit just about anywhere!",
+    "A cat’s and kitten’s whiskers are about as long as their bodies are wide. This is because whiskers are used to navigate through their surroundings and explore new places without your cat having to put her entire body there.",
+    "All kittens are born with blue eyes and will have their fully developed eye color at around 8 weeks old. Even the darkest eyed cats once had little blue eyes when they first opened.",
+    "Adult cats have 30 teeth, but kittens initially only have 26 teeth. They loose these baby teeth when they’re about 6 months old and the full set of 30 will grow in to replace them.",
+    "The average litter size for cats is about 1-9 kittens, but the largest ever recorded was a total of 19 kittens! That’s a lot of mouths to feed!",
+  ]
+  const rand = Math.floor((Math.random() * 10) + 1);
+  const randomKittenFact = kittenFacts[rand]
+  console.log(randomKittenFact)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="hero" ref={el => app = el}>
+      <div className="container">
+        <div className="hero-inner">
+          <div className="hero-content">
+          <div className="hero-content-inner" ref={el => content = el}>
+              <h1>
+                <div className="hero-content-line">
+                  <div className="hero-content-line-inner">Your next best friend</div>
+                </div>
+                <div className="hero-content-line">
+                  <div className="hero-content-line-inner">could well be</div>
+                </div>
+                <div className="hero-content-line">
+                  <div className="hero-content-line-inner">a kitten</div>
+                </div>
+              </h1>
+              <p>{randomKittenFact} - iheartcats.com</p>
+              <div className="btn-row">
+                <button className="explore-button">Explore
+                  <div className="arrow-icon">
+                    <img src={arrow} alt="row" />
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="hero-images">
+            <div ref={el => images = el} className="hero-images-inner">
+              <div className=" hero-image jinglebells">
+                <img src="https://brunswickcentralvet.com.au/sites/default/files/images/article/kitten-portrait.jpg" alt="mittons" />
+              </div>
+              <div className="hero-image mittons">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT26FxczkMhz9Fe-ZmB6FKj1jJ4fuFRQql1dQ&usqp=CAU" alt="jinglebells" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
